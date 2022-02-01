9576d39913cda18e01f1e0fb1b61d550b923f541
GRaonix <raoni.gillet@gmail.com>
2022-02-01 09:28:18 +0100

    adding progress bar

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+
 const app = Vue.createApp({
+
+
+
+
+    
     data() {
         return {
             cart: 0,
-            details: ['Cash/click : ','Cash/sec : ', 'Factories owned : ', 'Mega Factories owned : '],
+            details: ['Cash/click : ','Cash/sec : ', 'Factories owned : ', 'Mega Factories owned : ', 'Giga Factories owned : '],
             
         }
     },
